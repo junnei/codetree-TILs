dc056f14a1c31e1f50486a443e62e2c49dbab437
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-15 16:35:11 +0900

    Title: 4가지 관계연산자, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240115/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+a, b = tuple(map(int, input().split()))
+
+print(1 if a >= b else 0)
+print(1 if a > b else 0)
+print(1 if a <= b else 0)
+print(1 if a < b else 0)
