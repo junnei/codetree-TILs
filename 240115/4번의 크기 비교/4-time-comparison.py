2ee9a7d8e095f5df99d221cb10700d68b3f39e0b
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-15 16:37:25 +0900

    Title: 4번의 크기 비교, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240115/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+a = int(input())
+b, c, d, e = tuple(map(int, input().split()))
+
+
+print(1 if a > b else 0)
+print(1 if a > c else 0)
+print(1 if a > d else 0)
+print(1 if a > e else 0)
