3eebb2c9dbc0411d8bd8bd4c5324e1f9b15605b4
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-15 16:42:31 +0900

    Title: 특정 조건 세 정수 비교, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240115/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 240115/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -4,4 +4,4 @@ input = sys.stdin.readline
 
 a, b, c = tuple(map(int, input().split()))
 
-print(1 if a < b and a < c else 0, 1 if a == b == c else 0)
+print(1 if a < b and a < c else 0, 1 if a == b and a == c else 0)
