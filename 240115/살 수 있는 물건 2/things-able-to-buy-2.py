eda327da7029e33f3be827f1f24fb008f2fb119d
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-15 16:06:42 +0900

    Title: 살 수 있는 물건 2, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240115/살 수 있는 물건 2/things-able-to-buy-2.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+if n>= 3000:
+    print("book")
+elif n>=1000:
+    print("mask")
+elif n>=500:
+    print("pen")
+else:
+    print("no")
