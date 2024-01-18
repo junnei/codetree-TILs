e8303ab712980ba3e6ef067f400d0c0e2f05d6a4
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-18 23:55:48 +0900

    Title: 369 게임, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/369 게임/369-game.py
+++ 240118/369 게임/369-game.py
@@ -5,7 +5,11 @@ input = sys.stdin.readline
 n = int(input())
 
 for i in range(1, n+1):
-    if i%3==0 or ((i//10)%3 == 0 and i%10 != 0):
+    if i%3==0:
+        print(0, end=" ")
+    elif (i%10)%3 == 0 and i%10 != 0:
+        print(0, end=" ")
+    elif (i//10)%10%3 == 0 and i//10%10 != 0:
         print(0, end=" ")
     else:
         print(i, end=" ")
