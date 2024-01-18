4fae770db9d95be827dd97e73d4f5799d813edbf
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 00:32:40 +0900

    Title: 완전수 판별기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/완전수 판별기/perfect-number-discriminator.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+sum_val = 0
+
+for i in range(1,n):
+    if n % i == 0:
+        sum_val += i
+        
+print("P" if sum_val == n else "N")
