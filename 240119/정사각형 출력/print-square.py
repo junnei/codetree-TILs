74fa429d24cc76764311fa4c285092b9b4c5b577
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 21:31:09 +0900

    Title: 정사각형 출력, Time: 164ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240119/정사각형 출력/print-square.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        cnt += 1
+        print(cnt, end=" ")
+    print()
