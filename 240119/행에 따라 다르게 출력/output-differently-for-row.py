f520ede1eff364ae112c6d7ba691b6704ce5eb93
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 21:42:05 +0900

    Title: 행에 따라 다르게 출력, Time: 180ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240119/행에 따라 다르게 출력/output-differently-for-row.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i % 2 == 0:
+            cnt += 2
+        else:
+            cnt += 1
+        print(cnt, end=" ")
+    print()
