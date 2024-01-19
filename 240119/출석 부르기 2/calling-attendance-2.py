0765024f6551a97b419d1f22768e78857dbbbb28
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 18:24:49 +0900

    Title: 출석 부르기 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    n = int(input())
+    if n == 1:
+        print("John")
+    elif n == 2:
+        print("Tom")
+    elif n == 3:
+        print("Paul")
+    elif n == 4:
+        print("Sam")
+    else:
+        print("Vacancy")
+        break
