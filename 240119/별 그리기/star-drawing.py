ee7748644298d8d180d5e6a903252af512932370
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 20:00:34 +0900

    Title: 별 그리기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/별 그리기/star-drawing.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(2*n-1):
+    if i < n:
+        print(" "*(n-1-i)+"*"*(2*i+1))
+    else:
+        print(" "*(i-n+1)+"*"*((2*n-1-i)*2-1))
