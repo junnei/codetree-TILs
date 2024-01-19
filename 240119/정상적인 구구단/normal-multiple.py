91ed9d8ed3d5cb6ccbcf688e80b4490eb3fe597d
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 21:44:22 +0900

    Title: 정상적인 구구단, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/정상적인 구구단/normal-multiple.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(1, n+1): 
+    for j in range(1, n+1):
+        print(f"{i} * {j} = {i * j}", end="")
+        if j != n:
+            print(", ", end="")
+    print()
