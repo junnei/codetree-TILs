0e22fb717038ff5a5af9b78d56a52b9607fd510d
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 22:06:12 +0900

    Title: 구구단으로 이루어진 직각 삼각형, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(1, n+1): 
+    for j in range(1, n+1-i+1):
+        print(f"{i} * {j} = {i*j}", end="")
+        if j != n-i+1:
+            print(" / ", end="")
+        else:
+            print()
