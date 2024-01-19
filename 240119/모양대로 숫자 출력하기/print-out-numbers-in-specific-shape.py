4d6fdb44db895f857d38b8f0da6791e2ffeb2560
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 21:56:23 +0900

    Title: 모양대로 숫자 출력하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(n, 0, -1): 
+    print("  "*(n-i), end="")
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
