1d2a619dc362f16115a8d4f6318ca1e080942abf
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 19:48:36 +0900

    Title: 별표 출력하기 9, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/별표 출력하기 9/print-star-9.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(1,n+1):
+    for _ in range(n-i):
+        print("  ", end="")
+    for _ in range(i*2-1):
+        print("* ", end="")
+    print()
