c030d123ba981dde85882a1c7b65f23abf9492ac
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 19:17:05 +0900

    Title: 공약수의 유무, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240119/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -6,6 +6,6 @@ a, b = tuple(map(int,input().split()))
 found = False
 
 for i in range(a,b+1):
-    if 1920 % i == 0 or 2880 % i == 0:
+    if 1920 % i == 0 and 2880 % i == 0:
         found = True
 print(1 if found else 0)
