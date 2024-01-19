72c222bfc34cd79b78285c3dedfe494c48c98c52
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 22:10:43 +0900

    Title: 알파벳 출력하기 2, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240119/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+cnt = 65
+
+for i in range(1, n+1): 
+    for j in range(1, i):
+        print("  ", end="")
+    for j in range(i, n+1):
+        print(chr(cnt), end=" ")
+        cnt += 1
+    print()
