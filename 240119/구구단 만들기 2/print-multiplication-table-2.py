598dbff72d74029b3eaa4ea9e434b9ea4c6e173d
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 21:50:53 +0900

    Title: 구구단 만들기 2, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/구구단 만들기 2/print-multiplication-table-2.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+a, b = map(int,input().split())
+
+for i in range(2, 9, 2): 
+    for j in range(b, a-1, -1):
+        print(f"{j} * {i} = {i * j}", end="")
+        if j != a:
+            print(" / ", end="")
+    print()
