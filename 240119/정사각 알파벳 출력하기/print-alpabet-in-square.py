38c5a789e2fd919330a291fd945b1bf28ede2e8e
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 22:07:37 +0900

    Title: 정사각 알파벳 출력하기, Time: 218ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/정사각 알파벳 출력하기/print-alpabet-in-square.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+cnt = 65
+
+for i in range(1, n+1): 
+    for j in range(1, n+1):
+        print(chr(cnt), end="")
+        cnt += 1
+    print()
