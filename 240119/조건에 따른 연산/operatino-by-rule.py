5d7e7257c98c1037aa86416b06799741898adce5
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 18:50:42 +0900

    Title: 조건에 따른 연산, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240119/조건에 따른 연산/operatino-by-rule.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0
+
+while n<1000:
+    if n % 2 == 0:
+        n *= 3
+        n += 1
+    else:
+        n *= 2
+        n += 2
+    cnt += 1
+print(cnt)
