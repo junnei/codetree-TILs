85d4a71d8399847b8135fbe945ef3c7b350b1b0f
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 23:11:49 +0900

    Title: 소수 구하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240119/소수 구하기/get-prime.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(1, n+1):
+    for j in range(2, i+1):
+        if i % j == 0:
+            print(j, end=" ")
+            break
