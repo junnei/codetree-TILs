a1d8ab03baf5ef66f82ff9d4a9cb8e18b8077a00
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 19:27:56 +0900

    Title: 직사각형 별표 출력하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/직사각형 별표 출력하기/print-stars-in-rectangle.py
+++ 240119/직사각형 별표 출력하기/print-stars-in-rectangle.py
@@ -1,10 +1,4 @@
-import sys
-
-input = sys.stdin.readline
-
 n, m = map(int,input().split())
 
 for _ in range(n):
-    for _ in range(m):
-        print("*", end=" ")
-    print()
+    print("* "*m)
