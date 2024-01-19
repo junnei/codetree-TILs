96597d696c4544b0e9510c58e36ea9b5d2ff5f67
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-19 21:48:31 +0900

    Title: 구구단 만들기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/구구단 만들기/print-multiplication-table.py
+++ 240119/구구단 만들기/print-multiplication-table.py
@@ -10,6 +10,6 @@ if b%2 != 0:
 for i in range(1, 10): 
     for j in range(b, a-1, -2):
         print(f"{j} * {i} = {i * j}", end="")
-        if j != a or j != a+1:
+        if j != a and j != a+1:
             print(" / ", end="")
     print()
