029c9dddcccdc3b12e22dc4d7c1f554b331c0728
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-20 17:16:03 +0900

    Title: 짝수들을 거꾸로 출력, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240120/짝수들을 거꾸로 출력/print-even-numbers-upside-down.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(len(arr)-1, -1, -1):
+    if arr[i] % 2 == 0:
+        print(arr[i], end=" ")
