2a94aa52f27f8203e49ad256417cde912a9a9114
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-20 17:14:06 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240120/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0
+for _ in range(n):
+    arr = list(map(int, input().split()))
+    if sum(arr)/len(arr) >= 60:
+        print("pass")
+        cnt+=1
+    else:
+        print("fail")
+print(cnt)
