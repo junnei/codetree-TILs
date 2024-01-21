021c232df435a4153e4c9b1789b86cc320b73ebc
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-21 18:09:58 +0900

    Title: 동전이 있는 위치, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240121/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+result = [
+    [0] * (n+1)
+    for _ in range(n+1)
+]
+
+for _ in range(m):
+    r, c = list(map(int, input().split()))
+    result[r][c] = 1
+
+for i in range(1, n+1):
+    print(*result[i][1:])
