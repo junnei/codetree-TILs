a0592158080f164396baff773f513ac58f8c6ea3
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-21 16:25:49 +0900

    Title: 배열의 평균, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240121/배열의 평균/ave-of-array.py
+++ 240121/배열의 평균/ave-of-array.py
@@ -1,13 +1,5 @@
-import sys
-
-input = sys.stdin.readline
-
-n = 2
-size = 4
-arr = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
+n, size = 2, 4
+arr = [list(map(int, input().split())) for _ in range(n)]
 
 print(*[round(sum(arr[i]) / size, 1) for i in range(n)])
 print(*[round(sum([arr[i][j] for i in range(n)]) / n, 1) for j in range(size)])
