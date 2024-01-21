6c23c721b5609573e581b1b21f3ece3055dc8949
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-21 18:24:17 +0900

    Title: 가장 짧은 문자열, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240121/가장 짧은 문자열/shortest-string.py
+++ 240121/가장 짧은 문자열/shortest-string.py
@@ -3,6 +3,4 @@ import sys
 input = sys.stdin.readline
 
 arr = list(map(len, [input().rstrip() for _ in range(3)]))
-max_val = max(arr)
-min_val = min(arr)
-print(max_val - min_val)
+print(max(arr) - min(arr))
