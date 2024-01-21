c62416eddd3681f8e80fc1f683bc023d119c0dc3
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-21 15:21:49 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240121/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240121/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -10,11 +10,10 @@ result = [0]*1001
 for num in arr:
     result[num] += 1
 
-found = False
+answer = -1
 for i in range(1000, 0, -1):
     if result[i] == 1:
-        found = True
-        print(i)
+        answer = i
         break
-if not found:
-    print(-1)
+
+print(answer)
