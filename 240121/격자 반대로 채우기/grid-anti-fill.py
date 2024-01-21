623e6659f959469b53db75ebcf6f87f20a496f3f
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-21 17:54:02 +0900

    Title: 격자 반대로 채우기, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240121/격자 반대로 채우기/grid-anti-fill.py
+++ 240121/격자 반대로 채우기/grid-anti-fill.py
@@ -5,4 +5,4 @@ input = sys.stdin.readline
 n = int(input())
 
 for i in range(n,0,-1):
-    print(*[i+j*n if j % 2 == n % 2 else n+1-i+j*n for j in range(n-1,-1,-1)])
+    print(*[i+j*n if j % 2 == 0 else n+1-i+j*n for j in range(n-1,-1,-1)])
