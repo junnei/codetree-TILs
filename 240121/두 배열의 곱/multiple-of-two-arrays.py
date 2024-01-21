75a323b7b7ffbeb9456e1310ca675430006d6c6f
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-21 16:46:22 +0900

    Title: 두 배열의 곱, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240121/두 배열의 곱/multiple-of-two-arrays.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n = 3
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+_ = input()
+arr2 = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j] * arr2[i][j], end =" ")
+    print()
