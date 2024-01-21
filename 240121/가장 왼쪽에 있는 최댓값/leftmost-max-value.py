0e62c376231b7df3e3c5758b878ffa012604286e
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-21 15:37:45 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240121/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240121/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -4,16 +4,14 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
+max_val = -sys.maxsize
 
-while True:
-    max_val = -sys.maxsize
-    max_idx = 0
+result = []
 
-    for idx, num in enumerate(arr):
-        if num > max_val:
-            max_val = num
-            max_idx = idx
-    print(max_idx + 1, end=" ")
-    if max_idx == 0:
-        break
-    arr = arr[:max_idx]
+for idx, num in enumerate(arr):
+    if num > max_val:
+        result.append(idx+1)
+        max_val = num
+
+for i in result[::-1]:
+    print(i, end=" ")
