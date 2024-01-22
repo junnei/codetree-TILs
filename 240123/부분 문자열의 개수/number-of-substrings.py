276697fa86704db68164fbf75a6e76ed7ee8633d
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-23 01:04:41 +0900

    Title: 부분 문자열의 개수, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/부분 문자열의 개수/number-of-substrings.py
+++ 240123/부분 문자열의 개수/number-of-substrings.py
@@ -6,7 +6,7 @@ string = input().rstrip()
 sub = input().rstrip()
 cnt = 0
 
-for i in range(len(string)):
-    if string[i:].find(sub) == 0:
-        cnt += 1
+for i in range(len(string)-1):
+    if sub[0] == string[i] and sub[1] == string[i+1]:
+            cnt += 1
 print(cnt)
