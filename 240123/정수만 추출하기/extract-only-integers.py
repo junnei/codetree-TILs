d67ea302c95dcd7699fdbf60ca9afb2aa34791f8
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-23 16:58:10 +0900

    Title: 정수만 추출하기, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240123/정수만 추출하기/extract-only-integers.py
+++ 240123/정수만 추출하기/extract-only-integers.py
@@ -7,8 +7,12 @@ arr = input().rstrip().split()
 result = 0
 
 for string in arr:
+    found = False
     for i in range(len(string)):
         if not string[i].isdigit():
             result += int(string[:i])
+            found = True
             break
+    if found == False:
+        result += int(string[:i])
 print(result)
