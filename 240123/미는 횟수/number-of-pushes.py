dd61b82be5d1859d6050acacb9518bbb1bb6de62
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-23 17:22:03 +0900

    Title: 미는 횟수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240123/미는 횟수/number-of-pushes.py
+++ 240123/미는 횟수/number-of-pushes.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 a, b = [input().rstrip() for _ in range(2)]
 cnt = -1
 
-for i in range(1, len(a)):
+for i in range(1, len(a) + 1):
     if b == a[i:] + a[:i]:
         cnt = i
         break
