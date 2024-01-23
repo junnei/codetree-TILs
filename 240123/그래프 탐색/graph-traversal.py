c300931f806eabb4e84f89f03a005ffdb86691d1
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-23 18:01:09 +0900

    Title: 그래프 탐색, Time: 162ms, Memory: 26MB, Status: Passed - CodeTree

--- 240123/그래프 탐색/graph-traversal.py
+++ 240123/그래프 탐색/graph-traversal.py
@@ -3,21 +3,21 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-board = [[0] for _ in range(n+1)]
-is_visited = [0] * (n+1)
+board = [[] for _ in range(n+1)]
+is_visited = [False for _ in range(n+1)]
 
 for i in range(m):
     a, b = map(int, input().split())
     board[a].append(b)
     board[b].append(a)
 
-def dfs(x, board):
-    if is_visited[x] == 1:
+def dfs(x):
+    if is_visited[x] == True:
         return
-    is_visited[x] = 1
-    for i in board[x][1:]:
-        dfs(i, board)
+    is_visited[x] = True
+    for i in board[x]:
+        dfs(i)
 
-dfs(1, board)
+dfs(1)
 
 print(sum(is_visited[2:]))
