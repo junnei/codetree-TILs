141bd7d9a451f5d27a02967d918db43f8976e27f
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-26 16:40:02 +0900

    Title: 숫자로 이루어진 사각형, Time: 195ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240126/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,12 @@
+def print_rect(n):
+    cnt = 1
+    for i in range(n):
+        for j in range(n):
+            print(cnt, end=" ")
+            cnt+=1
+            if cnt == 10:
+                cnt = 1
+        print()
+
+n = int(input())
+print_rect(n)
