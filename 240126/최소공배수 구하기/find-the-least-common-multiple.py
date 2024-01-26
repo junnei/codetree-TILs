52a735631472ddc712547943f3e8377fb7469452
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-26 17:36:46 +0900

    Title: 최소공배수 구하기, Time: 239ms, Memory: 25MB, Status: Passed - CodeTree

--- 240126/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240126/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,17 +1,15 @@
 from functools import reduce
 
-def get_minimax(n, m, data):
-    found = False
-    for i in range(2, min(n, m) + 1):
+def get_gcd(n, m):
+    gcd = 0
+    for i in range(1, min(n, m) + 1):
         if n % i == 0 and m % i == 0:
-            found = True
-            get_minimax(n//i, m//i, data+[i])
-            break
-    if found == False:
-        if len(data) != 0:
-            print(n * m * reduce(lambda x, y: x * y, data))
-        else:
-            print(n * m)
+            gcd = i
+    return gcd
+
+def get_lcm(n, m):
+    return n * m // get_gcd(n, m)
 
 n, m = map(int, input().split())
-get_minimax(n, m, [])
+
+print(get_lcm(n, m))
