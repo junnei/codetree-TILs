49af05ae98cee5edfd3f05d7dabc910b2d0e30f0
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-28 14:39:31 +0900

    Title: 두 정수에 대한 연산값, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240128/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240128/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,9 +1,10 @@
 def solution(a, b):
-    low, high = (a, b) if a < b else (b, a)
+    is_right_order = a < b
+    low, high = (a, b) if is_right_order else (b, a)
 
     high += 25
     low *= 2
-    return low, high
+    return (low, high) if is_right_order else (high, low)
 
 a, b = map(int, input().split())
 
