d647388089db77426bec96499ab2479839c51542
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-28 15:05:39 +0900

    Title: 나누고 빼면서 합하기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240128/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -0,0 +1,15 @@
+def solution(n, m, arr):
+    result = arr[0]
+    while m != 1:
+        result += arr[m - 1]
+        if m % 2 == 1:
+            m -= 1
+        else:
+            m //= 2
+
+    return result
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+print(solution(n, m, arr))
