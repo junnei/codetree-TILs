c08e09e492782a6cdde46e5260e74e86489b6a4b
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-30 15:37:33 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 233ms, Memory: 56MB, Status: Passed - CodeTree

--- 240130/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240130/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -10,18 +10,20 @@ for idx, (x1, y1, x2, y2) in enumerate(datas):
         for y in range(y1, y2):
             result[x][y] = 1 - idx
 
-
 min_x, min_y, max_x, max_y = 2000, 2000, 0, 0
-for i, row in enumerate(result):
-    for j, val in enumerate(row):
-        if val == 1:
-            if i < min_x:
-                min_x = i
-            if i > max_x:
-                max_x = i
-            if j < min_y:
-                min_y = j
-            if j > max_y:
-                max_y = j
+if sum([sum(row) for row in result]) == 0:
+    print(0)
+else:
+    for i, row in enumerate(result):
+        for j, val in enumerate(row):
+            if val == 1:
+                if i < min_x:
+                    min_x = i
+                if i > max_x:
+                    max_x = i
+                if j < min_y:
+                    min_y = j
+                if j > max_y:
+                    max_y = j
 
-print((max_x-min_x+1)*(max_y-min_y+1))
+    print((max_x-min_x+1)*(max_y-min_y+1))
