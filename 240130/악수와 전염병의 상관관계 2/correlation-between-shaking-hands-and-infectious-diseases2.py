bc18d0e423efebb7806fcf3154c21f7738af00d6
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-30 18:11:50 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240130/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -10,15 +10,18 @@ infect[P] = K+1
 
 for t, x, y in queries:
     if infect[x] >= 2:
+        infect[x] -= 1
+        
         if infect[y] >= 2:
-            infect[x] = K+1
+            infect[y] -= 1
+        elif infect[y] == 0:
             infect[y] = K+1
-        else:
-            infect[x] -= 1
-            infect[y] = K+1
-    elif infect[y] >= 2:
-        infect[y] -= 1
-        infect[x] = K+1
+    else:
+        if infect[y] >= 2:
+            infect[y] -= 1
+            
+            if infect[x] == 0:
+                infect[x] = K+1
 
 for i in infect[1:]:
     print(0 if i == 0 else 1, end="")
