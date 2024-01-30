82434860063994927216c5c3983efdbf866ed5b5
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-30 14:36:01 +0900

    Title: 흰검 칠하기, Time: 163ms, Memory: 38MB, Status: Wrong Answer - CodeTree

--- 240130/흰검 칠하기/painting-white-black.py
+++ 240130/흰검 칠하기/painting-white-black.py
@@ -6,16 +6,16 @@ result = [[0, 0, 0] for _ in range(200000+1)]
 idx = 100000
 for val, command in datas:
     if command == "L":
-        for i in range(idx-int(val), idx+1):
+        for i in range(idx-int(val), idx):
             result[i][0] += 1
             result[i][2] = 0
         idx-=int(val)
     else:
-        for i in range(idx, idx+int(val)+1):
+        for i in range(idx, idx+int(val)):
             result[i][1] += 1
             result[i][2] = 1
         idx+=int(val)
-
+        
 sum_val = [0, 0, 0]
 for x, y, color in result:
     if x >= 2 and y >= 2:
