6a2393d61906a28a981c7ab7906e27a252f791c4
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-30 13:45:43 +0900

    Title: 요일 맞추기, Time: 134ms, Memory: 73MB, Status: Passed - CodeTree

--- 240130/요일 맞추기/guess-day-of-week.py
+++ 240130/요일 맞추기/guess-day-of-week.py
@@ -5,4 +5,4 @@ m1, d1, m2, d2 = map(int, input().split())
 if m1<m2 or (m1==m2 and d1<=d2):
     print(days[((sum(num_of_days[m1:m2])+(d2-d1))+1)%7])
 else:
-    print(days[((sum(num_of_days[m2:m1])+(d1-d2))-1)%7])
+    print(days[(-(sum(num_of_days[m2:m1])+(d1-d2))+1)%7])
