b89fb8117a82eab1b18e086bd87971cd1b0a5f7a
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-30 14:47:29 +0900

    Title: 신기한 타일 뒤집기, Time: 186ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 240130/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+datas = [input().split() for _ in range(n)]
+idx = 100000
+result = [0 for _ in range(2*idx+1)]
+
+for val, command in datas:
+    if command == "L":
+        for i in range(idx-int(val)+1, idx+1):
+            result[i] = 1
+        idx-=int(val)-1
+    else:
+        for i in range(idx, idx+int(val)):
+            result[i] = 2
+        idx+=int(val)-1
+
+sum_val = [0, 0, 0]
+for data in result:
+    if data != 0:
+        sum_val[data] += 1
+print(*sum_val[1:])
