fe5d682fc25e52624d3c79086c96fe598630ae4e
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-31 03:04:53 +0900

    Title: 거울에 레이저 쏘기 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240131/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240131/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -2,24 +2,13 @@ n = int(input())
 
 arr = [list(input()) for _ in range(n)]
 
-k = int(input())
+k = int(input()) - 1
 
-directions = {
-    1 : ([1, 1], [1, 0]),
-    2 : ([1, 2], [1, 0]),
-    3 : ([1, 3], [1, 0]),
-    4 : ([1, 3], [0, -1]),
-    5 : ([2, 3], [0, -1]),
-    6 : ([3, 3], [0, -1]),
-    7 : ([3, 3], [-1, 0]),
-    8 : ([3, 2], [-1, 0]),
-    9 : ([3, 1], [-1, 0]),
-    10 : ([3, 1], [0, 1]),
-    11 : ([2, 1], [0, 1]),
-    12 : ([1, 1], [0, 1]),
-}
-
-(x, y), (dx, dy) = directions[k]
+dir_num, index = k // n, k % n
+dxys = [[1, 0], [0, -1], [-1, 0], [0, 1]]
+start_point = [[1, 1+index], [1+index, 3], [3, 3-index], [3-index, 1]]
+x, y = start_point[dir_num]
+dx, dy = dxys[dir_num]
 
 def is_range(x, y):
     return 1<=x and x<=n and 1<=y and y<=n
