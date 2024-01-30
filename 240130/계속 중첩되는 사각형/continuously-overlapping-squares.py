a743cdc0eb2f941c4e998eb7da2d7aa3190c03d2
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-30 15:44:29 +0900

    Title: 계속 중첩되는 사각형, Time: 172ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240130/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -0,0 +1,30 @@
+OFFSET = 100
+result = [[0 for _ in range(2*OFFSET+1)] for _ in range(2*OFFSET+1)]
+
+n = int(input())
+datas = [map(int, input().split()) for _ in range(n)]
+
+for idx, (x1, y1, x2, y2) in enumerate(datas):
+    x1, y1 = x1+OFFSET, y1+OFFSET
+    x2, y2 = x2+OFFSET, y2+OFFSET
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            result[x][y] = idx % 2
+
+min_x, min_y, max_x, max_y = 200, 200, 0, 0
+if sum([sum(row) for row in result]) == 0:
+    print(0)
+else:
+    for i, row in enumerate(result):
+        for j, val in enumerate(row):
+            if val == 1:
+                if i < min_x:
+                    min_x = i
+                if i > max_x:
+                    max_x = i
+                if j < min_y:
+                    min_y = j
+                if j > max_y:
+                    max_y = j
+
+    print((max_x-min_x+1)*(max_y-min_y+1))
