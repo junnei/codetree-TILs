b7773973900e57fe6f71292f55f334f85f2551ff
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-01-31 20:43:43 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 139ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240131/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -0,0 +1,33 @@
+n = int(input())
+
+arr = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+def in_range(x, y):
+    return 0<=x and x<n and 0<=y and y<n
+
+def move(x, y, direction, cnt):
+    arr[x][y] = cnt
+    nx, ny = x + dxs[direction], y + dys[direction]
+
+    if in_range(nx, ny) and arr[nx][ny] == 0:
+        return nx, ny, direction, cnt + 1
+    else:
+        nd = (direction + 1) % 4
+        nx, ny = x + dxs[nd], y + dys[nd]
+        return nx, ny, nd, cnt + 1
+
+dxs = [0, -1, 0, 1]
+dys = [1, 0, -1, 0]
+
+def simulate(x, y, direction, cnt):
+    while in_range(x, y) and arr[x][y] == 0:
+        x, y, direction, cnt = move(x, y, direction, cnt)
+
+x, y, direction = n//2, n//2, 0
+simulate(x, y, direction, 1)
+
+for col in arr:
+    print(*col)
