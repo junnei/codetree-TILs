475d8549f0fe81d4c8599398241fd17f1a8ff037
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-01 21:09:21 +0900

    Title: 바구니 안의 사탕 2, Time: 111ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240201/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,22 @@
+import sys 
+
+MAX_POS = 101
+
+n, k = map(int, input().split())
+arr = [0 for _ in range(MAX_POS)]
+
+for i in range(n):
+    val, pos = map(int, input().split())
+    arr[pos] = val
+
+import sys
+
+MIN_INT = -sys.maxsize
+
+max_val = MIN_INT
+
+for i in range(k, MAX_POS-k):
+    cnt = sum(arr[i-k:i+k+1])
+    if cnt > max_val:
+        max_val = cnt
+print(max_val)
