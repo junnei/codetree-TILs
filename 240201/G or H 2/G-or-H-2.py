237684274886ed4b84e006b8e85be04507207fac
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-01 21:49:07 +0900

    Title: G or H 2, Time: 175ms, Memory: 25MB, Status: Passed - CodeTree

--- 240201/G or H 2/G-or-H-2.py
+++ 240201/G or H 2/G-or-H-2.py
@@ -5,12 +5,11 @@ arr = [0 for _ in range(MAX_POS+1)]
 for _ in range(n):
     pos, val = input().split()
     arr[int(pos)] = 1 if val == "G" else 2
-
 max_val = 0
 for i in range(MAX_POS):
     for j in range(i+1, MAX_POS+1):
         cnt_g, cnt_h = 0, 0
-        max_idx, min_idx = 0, 100
+        max_idx, min_idx = 0, 101
         for idx, item in enumerate(arr[i:j+1]):
             if item == 1:
                 cnt_g += 1
@@ -24,7 +23,7 @@ for i in range(MAX_POS):
                     max_idx = idx
                 if idx < min_idx:
                     min_idx = idx
-        if cnt_g + cnt_h == j-i or cnt_g == cnt_h:
+        if cnt_g == j-i+1 or cnt_h == j-i+1 or cnt_g == cnt_h:
             if max_idx - min_idx > max_val:
                 max_val = max_idx - min_idx
 print(max_val)
