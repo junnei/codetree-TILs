e48d494b36624b037adcb7fcc50cecd82a0750fe
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-01 21:03:49 +0900

    Title: 아름다운 수열 2, Time: 223ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/아름다운 수열 2/beautiful-sequence-2.py
+++ 240201/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,18 +1,23 @@
 import sys 
 
+MAX_SIZE = 100
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
-b = list(map(int, input().split()))
+b = [0] * (MAX_SIZE + 1)
+
+for i in list(map(int, input().split())):
+    b[i] += 1
 
 if n<m:
     print(0)
     sys.exit()
-from itertools import permutations
-arr = list(map(list, permutations(b)))
 
 cnt = 0
 
 for i in range(n-m+1):
-    if a[i:i+m] in arr:
+    temp = [0] * (MAX_SIZE + 1)
+    for item in a[i:i+m]:
+        temp[item] += 1
+    if temp == b:
         cnt += 1
 print(cnt)
