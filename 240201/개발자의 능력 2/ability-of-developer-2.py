e6da045c6dc2167411f796e423cce80edee9f73b
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-01 22:57:25 +0900

    Title: 개발자의 능력 2, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/개발자의 능력 2/ability-of-developer-2.py
+++ 240201/개발자의 능력 2/ability-of-developer-2.py
@@ -10,7 +10,7 @@ for i in range(n-1):
     for j in range(i+1, n):
         for k in range(n-3):
             for l in range(k+1, n-2):
-                temp_arr = (arr[:i]+arr[i+1:j]+arr[j:])
+                temp_arr = (arr[:i]+arr[i+1:j]+arr[j+1:])
                 a = (arr[i] + arr[j])
                 b = (temp_arr[k] + temp_arr[l])
                 c = sum_val - a - b
