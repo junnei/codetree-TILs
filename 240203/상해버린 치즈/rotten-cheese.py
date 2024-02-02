59ee1d62631b4c1afc8178d089aa39e59b40c5d5
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-03 00:46:08 +0900

    Title: 상해버린 치즈, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240203/상해버린 치즈/rotten-cheese.py
+++ 240203/상해버린 치즈/rotten-cheese.py
@@ -1,22 +1,30 @@
 n, m, d, s = map(int, input().split())
 eats = [
-    tuple(map(int, input().split()))
-    for _ in range(d)
+    []
+    for _ in range(n)
 ]
+
+for _ in range(d):
+    p, m, t = tuple(map(int, input().split()))
+    eats[p-1].append([m, t])
+
 ills = [
     tuple(map(int, input().split()))
     for _ in range(s)
 ]
 
-eats.sort(lambda x: x[-1])
+for i in range(n):
+    eats[i].sort(lambda x: x[-1])
 ills.sort(lambda x: x[-1])
 
-cheeze = set()
-for ill_p, ill_t in ills:
-    for eat_p, eat_m, eat_t in eats:
-        if eat_t > ill_t:
-            break
-        if ill_p == eat_p:
-            cheeze.add(eat_m)
 
-print(len(cheeze))
+answer = set(range(1, m+1))
+
+for ill_p, ill_t in ills:
+    cheeze = set()
+    for eat_m, eat_t in eats[ill_p-1]:
+        if eat_t >= ill_t:
+            break
+        cheeze.add(eat_m)
+    answer = answer & cheeze
+print(len(answer))
