91c24784d383537a1dacff8f25b10ac95842be86
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-03 00:55:37 +0900

    Title: 상해버린 치즈, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/상해버린 치즈/rotten-cheese.py
+++ 240203/상해버린 치즈/rotten-cheese.py
@@ -1,24 +1,23 @@
-n, m, d, s = map(int, input().split())
+N, M, D, S = map(int, input().split())
 eats = [
     []
-    for _ in range(n)
+    for _ in range(N)
 ]
 
-for _ in range(d):
+for _ in range(D):
     p, m, t = tuple(map(int, input().split()))
     eats[p-1].append([m, t])
 
 ills = [
     tuple(map(int, input().split()))
-    for _ in range(s)
+    for _ in range(S)
 ]
 
-for i in range(n):
+for i in range(N):
     eats[i].sort(lambda x: x[-1])
 ills.sort(lambda x: x[-1])
 
-
-answer = set(range(1, m+1))
+answer = set(range(1, M+1))
 
 for ill_p, ill_t in ills:
     cheeze = set()
