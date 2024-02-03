3fc5ee921e58db32bb576bd4fafbddb369f372bc
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-03 18:27:29 +0900

    Title: 스승의 은혜 3, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240203/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,22 @@
+n, b = map(int, input().split())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+max_val = 0
+
+for i in range(n):
+    temp = arr[:]
+    temp[i][0] //= 2
+    temp.sort(lambda x : x[0]+x[1])
+
+    sum_val = 0
+    cnt = 0
+    for j in range(n):
+        sum_val += temp[j][0] + temp[j][1]
+        if sum_val >= b:
+            break
+        cnt += 1
+    max_val = max(max_val, cnt)
+print(max_val)
