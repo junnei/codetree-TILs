ffa6474cba6d715246c3c8b02f5def240e6a49dd
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-03 18:02:17 +0900

    Title: 이상한 폭탄 2, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/이상한 폭탄 2/strange-bomb-2.py
+++ 240203/이상한 폭탄 2/strange-bomb-2.py
@@ -2,7 +2,7 @@ n, k = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 
 max_num = -1
-for i in range(n-k):
-    if arr[i] in arr[i+1:i+k+1]:
+for i in range(n):
+    if arr[i] in arr[i+1:min(i+k+1, n)]:
         max_num = max(max_num, arr[i])
 print(max_num)
