a6a4cd753dbf46f2354abc0cb668dde948a8d477
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-03 18:17:26 +0900

    Title: 선분 3개 지우기, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240203/선분 3개 지우기/remove-three-segments.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+cnt = 0
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            found = True
+            line = [False] * n
+            for idx in range(n):
+                if idx == i or idx == j or idx == k:
+                    continue
+                a, b = arr[idx]
+                if True in line[a-1:b+1-1]:
+                    found = False
+                    break
+                line[a-1:b+1-1] = [True] * (b-a+1)
+            if found:
+                cnt += 1
+print(cnt)
