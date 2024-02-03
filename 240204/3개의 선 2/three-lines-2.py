0680e0f0bc69151b7a56e65193fdd76f93a86365
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-04 05:29:55 +0900

    Title: 3개의 선 2, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240204/3개의 선 2/three-lines-2.py
@@ -0,0 +1,29 @@
+n = int(input())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+from itertools import combinations
+
+cnt = 0
+dxs = [0, 1, 2, 3]
+dys = [3, 2, 1, 0]
+for dx, dy in zip(dxs, dys):
+    for x in list(combinations(range(11), dx)):
+        for y in list(combinations(range(11), dy)):
+            found = True
+            for i, j in arr:
+                if i in x or j in y:
+                    pass
+                else:
+                    found = False
+                    break
+            if found:
+                cnt+=1
+                break
+        if found: break
+if cnt:
+    print(1)
+else:
+    print(0)
