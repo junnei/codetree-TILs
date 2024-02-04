67aaebe0298a71dcb33d668708fc9693c09f718b
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-05 03:44:58 +0900

    Title: 초기 수열 복원하기, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240205/초기 수열 복원하기/restore-initial-sequence.py
+++ 240205/초기 수열 복원하기/restore-initial-sequence.py
@@ -1,15 +1,13 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-
-
 def solution():
     for start_num in range(1, arr[0]):
         found = True
         result = [start_num]
         for i in range(n-1):
             num = arr[i] - result[i]
-            if num <= 0 or num in result:
+            if num <= 0 or num > n or num in result:
                 found = False
                 break
             result.append(num)
