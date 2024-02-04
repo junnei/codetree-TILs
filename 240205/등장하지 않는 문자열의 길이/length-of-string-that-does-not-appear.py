b6d7c0c5bd26c2e3b4a6fe2a7240544274fcc930
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-05 00:59:28 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240205/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240205/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -5,13 +5,11 @@ arr = input()
 for len_str in range(1, n+1):
     found = False
     for i in range(n-len_str+1):
-        cnt = 0
-        for j in range(n-len_str+1):
+        for j in range(i+1, n-len_str+1):
             if arr[i:i+len_str] == arr[j:j+len_str]:
-                cnt += 1
-                if cnt >= 2:
-                    break
-        if cnt >= 2:
+                found = True
+                break
+        if found:
             found = True
             break
     if not found:
