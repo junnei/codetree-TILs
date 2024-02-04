05cbe0393e5b1808f80a4b6d3e1cd2a49834e5e1
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-05 03:42:10 +0900

    Title: 초기 수열 복원하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240205/초기 수열 복원하기/restore-initial-sequence.py
+++ 240205/초기 수열 복원하기/restore-initial-sequence.py
@@ -1,16 +1,18 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-from itertools import permutations
+
 
 def solution():
-    for nums in permutations(range(1,n+1)):
+    for start_num in range(1, arr[0]):
         found = True
-        for i in range(1, n):
-            if arr[i-1] != nums[i]+nums[i-1]:
+        result = [start_num]
+        for i in range(n-1):
+            num = arr[i] - result[i]
+            if num < 0 or num in result:
                 found = False
                 break
+            result.append(num)
         if found:
-            return nums
-
+            return result
 print(*solution())
