046323b2693590250ea918e81774c8a7cdefe394
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-05 05:58:29 +0900

    Title: 이상한 폭탄 3, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240205/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,32 @@
+n, k = map(int, input().split())
+arr = [
+    int(input())
+    for _ in range(n)
+]
+
+max_val = 0
+max_int = 0
+
+for i in range(n):
+    num = arr[i]
+    d = 0
+    j = 1
+    cnt = 0
+    while d <= k:
+        if i+j >= n:
+            break
+        if arr[i+j] == num:
+            cnt += 1
+            d = 0
+        else:
+            d += 1
+        j += 1
+
+    if cnt > max_val:
+        max_val = cnt
+        max_int = num
+
+if max_val == 0:
+    print(0)
+else:
+    print(max_int)
