bc32050e33b92bb5a2bbbf1dfea1595bb7dc628f
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-05 00:41:19 +0900

    Title: 팀으로 하는 틱택토 2, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240205/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -10,17 +10,21 @@ import sys
 def can_win(a, b):
     for i in range(3):
         if arr[i][0] in (a, b) and arr[i][1] in (a, b) and arr[i][2] in (a, b):
-            return True
+            if not arr[i][0] == arr[i][1] == arr[i][2]:
+                return True
 
     for i in range(3):
         if arr[0][i] in (a, b) and arr[1][i] in (a, b) and arr[2][i] in (a, b):
-            return True
+            if not arr[0][i] == arr[1][i] == arr[2][i]:
+                return True
 
     if arr[0][0] in (a, b) and arr[1][1] in (a, b) and arr[2][2] in (a, b):
-        return True
+        if not arr[0][0] == arr[1][1] == arr[2][2]:
+            return True
      
     if arr[2][0] in (a, b) and arr[1][1] in (a, b) and arr[0][2] in (a, b):
-        return True
+        if not arr[2][0] == arr[1][1] == arr[0][2]:
+            return True
     return False
 
 cnt = 0
