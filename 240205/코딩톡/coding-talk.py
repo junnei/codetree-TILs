df527bbfb35d8633673dafc7c8ca13d2706979ff
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-05 18:10:45 +0900

    Title: 코딩톡, Time: 117ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240205/코딩톡/coding-talk.py
@@ -0,0 +1,15 @@
+n, m, p = map(int, input().split())
+arr = [
+    input().split()
+    for _ in range(m)
+]
+
+people = [0] * n
+
+for i in range(p-1, m):
+    people[ord(arr[i][0])-65] = 1
+
+
+for i in range(n):
+    if people[i] == 0:
+        print(chr(i+65), end=" ")
