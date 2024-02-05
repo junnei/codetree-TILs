e0682932065a2b7b2b457e0fc4a55fca6a310108
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-05 18:16:56 +0900

    Title: 코딩톡, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240205/코딩톡/coding-talk.py
+++ 240205/코딩톡/coding-talk.py
@@ -7,10 +7,9 @@ arr = [
 people = [0] * n
 
 if arr[p-1][1] != '0':
-    for i in range(p-1, m):
-        people[ord(arr[i][0])-65] = 1
-
+    for i in range(m):
+        people[ord(arr[i][0])-65] = i + 1
 
     for i in range(n):
-        if people[i] == 0:
+        if people[i] < p-1:
             print(chr(i+65), end=" ")
