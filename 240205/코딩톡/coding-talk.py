9b2adba08e2ecb1260a2391ee424d43ee738b3cb
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-05 18:24:58 +0900

    Title: 코딩톡, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/코딩톡/coding-talk.py
+++ 240205/코딩톡/coding-talk.py
@@ -5,11 +5,13 @@ arr = [
 ]
 
 people = [0] * n
-
+to_find = 0
 if arr[p-1][1] != '0':
     for i in range(m):
-        people[ord(arr[i][0])-65] = i + 1
+        people[ord(arr[i][0])-65] = int(arr[i][1]) + 1
+        if i == p-1:
+            to_find = int(arr[i][1]) + 1
 
-    for i in range(n):
-        if people[i] < p-1:
-            print(chr(i+65), end=" ")
+for i in range(n):
+    if people[i] < to_find:
+        print(chr(i+65), end=" ")
