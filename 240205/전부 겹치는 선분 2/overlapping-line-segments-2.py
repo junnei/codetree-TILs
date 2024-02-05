7ff214157ba48248df999b7fdc3ee31cca5fb088
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-05 17:49:53 +0900

    Title: 전부 겹치는 선분 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240205/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -0,0 +1,25 @@
+n = int(input())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+def solution(arr):
+    min_end = 100
+    max_start = 0
+    for a, b in arr:
+        min_end = min(min_end, b)
+        max_start = max(max_start, b)
+        
+    if max_start <= min_end:
+        return True
+    return False
+
+found = False
+for i in range(n):
+    if solution(arr[:i] + arr[i+1:]):
+        found = True
+if found:
+    print("Yes")
+else:
+    print("No")
