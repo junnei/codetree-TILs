0c6b22cccbb800d72b6901274ba24d70176da427
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-06 04:16:51 +0900

    Title: 트로미노, Time: 648ms, Memory: 28MB, Status: Passed - CodeTree

--- 240206/트로미노/tromino.py
+++ 240206/트로미노/tromino.py
@@ -35,9 +35,9 @@ def get_max(arr):
     arr[1][2] + arr[2][2] + arr[2][1],
     arr[2][0] + arr[2][1] + arr[2][2]
     )
-
+    
 max_val = 0
 for i in range(n-2):
-    for j in range(n-2):
+    for j in range(m-2):
         max_val = max(max_val, get_max([row[j:j+3] for row in arr[i:i+3]]))
 print(max_val)
