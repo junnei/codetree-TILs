33a9c486ca65e9a289080f7b7b4e03d83c9c4b1a
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-05 20:06:18 +0900

    Title: 연속된 숫자 만들기 3, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 240205/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -3,7 +3,5 @@ arr.sort()
 
 if arr[2] - arr[1] == arr[1] - arr[0] == 1:
     print(0)
-elif arr[2] - arr[1] == 1 or arr[1] - arr[0] == 1:
-    print(arr[2]-arr[0]-2)
 else:
-    print(arr[2]-arr[0]-3)
+    print(max(arr[2]-arr[1], arr[1]-arr[0])-1)
