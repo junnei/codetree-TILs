41655ba1d10a280178d7c331ea1ff20cc13c86dd
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-06 04:20:15 +0900

    Title: 트로미노, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240206/트로미노/tromino.py
+++ 240206/트로미노/tromino.py
@@ -12,32 +12,11 @@ def get_max(arr):
     arr[0][0] + arr[0][1] + arr[1][1],
     arr[0][0] + arr[1][0] + arr[1][1],
     arr[0][0] + arr[1][0] + arr[2][0],
-
-    arr[0][1] + arr[0][2] + arr[1][1],
-    arr[0][1] + arr[0][2] + arr[1][2],
-    arr[0][1] + arr[1][1] + arr[1][0],
-    arr[0][1] + arr[1][1] + arr[1][2],
-    arr[0][1] + arr[1][1] + arr[2][1],
-
-    arr[0][2] + arr[1][2] + arr[1][1],
-    arr[0][2] + arr[1][2] + arr[2][2],
-
-    arr[1][0] + arr[1][1] + arr[2][0],
-    arr[1][0] + arr[1][1] + arr[2][1],
-    arr[1][0] + arr[1][1] + arr[1][2],
-    arr[1][0] + arr[2][0] + arr[2][1],
-
-    arr[1][1] + arr[1][2] + arr[2][2],
-    arr[1][1] + arr[1][2] + arr[2][1],
-    arr[1][1] + arr[2][1] + arr[2][0],
-    arr[1][1] + arr[2][1] + arr[2][2],
-
-    arr[1][2] + arr[2][2] + arr[2][1],
-    arr[2][0] + arr[2][1] + arr[2][2]
+    arr[0][1] + arr[1][1] + arr[1][0]
     )
-    
+
 max_val = 0
-for i in range(n-2):
-    for j in range(m-2):
+for i in range(n):
+    for j in range(m):
         max_val = max(max_val, get_max([row[j:j+3] for row in arr[i:i+3]]))
 print(max_val)
