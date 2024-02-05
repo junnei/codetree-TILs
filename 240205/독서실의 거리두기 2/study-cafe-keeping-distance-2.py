2dcc1e72fe38facd80b21e2e971186f27e8c6843
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-05 20:33:45 +0900

    Title: 독서실의 거리두기 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240205/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,26 @@
+n = int(input())
+arr = list(map(int, list(input())))
+
+dist = []
+
+first_index = 0
+last_index = -1
+for i in range(n):
+    if arr[i] == 1:
+        if last_index == -1:
+            first_index = i
+            last_index = i
+        else:
+            dist.append(i - last_index)
+            last_index = i
+
+last_index = n - 1 - last_index
+
+max_val = max(dist)
+if max_val//2 >= first_index - 1 or max_val//2 >= last_index - 1:
+    val = dist.pop(dist.index(max_val))
+    dist.extend([val-val//2, val//2])
+    print(min(dist))
+else:
+    new_dist = max(first_index, last_index) - 1
+    print(min(dist+[new_dist]))
