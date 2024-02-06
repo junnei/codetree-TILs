717836d40b995f7b1df1116190a2907f66d87b5f
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-06 17:37:10 +0900

    Title: 기울어진 직사각형, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240206/기울어진 직사각형/slanted-rectangle.py
@@ -0,0 +1,32 @@
+n = int(input())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+dxs = [-1, -1, 1, 1]
+dys = [1, -1, -1, 1]
+
+
+def get_sum_num(x, y, i, j):
+    sum_val = 0
+    cnts = [i, j, i, j]
+    for dx, dy, cnt in zip(dxs, dys, cnts):
+        for _ in range(cnt):
+            x, y = x + dx, y + dy
+            sum_val += arr[x][y]
+    return sum_val
+
+def get_max_num(a, b):
+    max_val = 0
+    for i in range(1, n):
+        for j in range(1, n):
+            if 0 <= a-(i+j) < n and max(i, j) <= b < n-max(i, j):
+                max_val = max(max_val, get_sum_num(a, b, i, j))
+    return max_val
+
+max_val = 0
+for i in range(2, n):
+    for j in range(1, n-1):
+        max_val = max(max_val, get_max_num(i, j))
+print(max_val)
