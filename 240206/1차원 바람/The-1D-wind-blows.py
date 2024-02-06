12f00baf90fdb9c26c8460d950b88fb24e1090bc
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-02-06 23:59:33 +0900

    Title: 1차원 바람, Time: 429ms, Memory: 31MB, Status: Passed - CodeTree

--- 240206/1차원 바람/The-1D-wind-blows.py
+++ 240206/1차원 바람/The-1D-wind-blows.py
@@ -9,6 +9,8 @@ commands = [
 ]
 
 def next_available(i, d, arr, v):
+    if not 0<=i+v<n:
+        return
     found = False
     for a, b in zip(arr[i], arr[i+v]):
         if a==b:
@@ -28,8 +30,7 @@ def move(i, d, arr, v):
         for v in [-1, 1]:
             next_available(i, d, arr, v)
     else:
-        if 0<=i+v<n:
-            next_available(i, d, arr, v)
+        next_available(i, d, arr, v)
         
 for r, d in commands:
     move(int(r)-1, d, arr, 0)
