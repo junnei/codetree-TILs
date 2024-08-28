b8e3c8886184caff7b5a2307ac5445ae01beec76
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-08-28 16:40:03 +0900

    Title: 최소 차 분할, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240828/최소 차 분할/minimum-diff-partition.py
+++ 240828/최소 차 분할/minimum-diff-partition.py
@@ -1,8 +1,7 @@
 n = int(input())
 nums = list(map(int, input().split()))
 
-MAX_VAL = sum(nums)//2
-remain = sum(nums)%2
+MAX_VAL = sum(nums)
 # n개의 수가 주어졌을 때, 이를 둘로 나눈 합 간의 차이
 # dp[n] : 무게의 합이 M일 때 최대의 가치.
 dp = [
@@ -16,10 +15,10 @@ for num in nums:
     if dp[num] == 0:
         dp[num] = 1
         
-answer = remain
-for val in dp[::-1]:
-    if val == 0:
-        answer += 1
-    else:
-        break
+answer = MAX_VAL
+for i, num in enumerate(dp):
+    if num != 0 and num != n:
+        if i < (MAX_VAL - i):
+            continue
+        answer = min(answer, i - (MAX_VAL - i))
 print(answer)
