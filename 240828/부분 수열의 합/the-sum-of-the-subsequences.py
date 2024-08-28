da57c94fe99ca2e8ad2ce5ebfc86a50147012a46
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-08-28 14:33:41 +0900

    Title: 부분 수열의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240828/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+
+dp = [
+    0 for _ in range(m+1)
+]
+
+for num in nums:
+    for i in range(m, num, -1):
+        if dp[i-num] != 0:
+            dp[i] = max(dp[i], dp[i-num] + 1)
+    dp[num] += 1
+print("No" if dp[-1] == 0 else "Yes")
