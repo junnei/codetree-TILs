8be4b611817e5313f0b48e3bda4a2347a98504d0
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2024-08-28 14:34:24 +0900

    Title: 부분 수열의 합, Time: 160ms, Memory: 72MB, Status: Passed - CodeTree

--- 240828/부분 수열의 합/the-sum-of-the-subsequences.py
+++ 240828/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -9,5 +9,6 @@ for num in nums:
     for i in range(m, num, -1):
         if dp[i-num] != 0:
             dp[i] = max(dp[i], dp[i-num] + 1)
-    dp[num] += 1
+    if num <= m:
+        dp[num] += 1
 print("No" if dp[-1] == 0 else "Yes")
